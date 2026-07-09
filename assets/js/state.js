7726f1f04e7513a19ebe6a685d7fd1f46de8e3b2
ufouapnews-dev <ufo.uap.news@gmail.com>
2026-07-09 12:43:09 -0600

    Update state.js

--- assets/js/state.js
+++ assets/js/state.js
@@ -1,1 +1,39 @@
-// App State
+const AppState = {
+
+    app: {
+
+        name: "Recuerdos",
+
+        version: "0.2.0"
+
+    },
+
+    event: {
+
+        name: "XV Años Sofía",
+
+        date: "29 Agosto 2026",
+
+        slogan:
+        "Comparte los momentos que harán inolvidable este día.",
+
+        heroImage:
+        "assets/images/hero.jpg"
+
+    },
+
+    navigation: {
+
+        currentPage: "home"
+
+    },
+
+    upload: {
+
+        section: null,
+
+        files: []
+
+    }
+
+};
